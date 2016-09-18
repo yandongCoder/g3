8a4e35e069758f657cd9f7da9d576973224d1821
yandong <470604226@qq.com>
2016-09-18 18:52:04 +0800

    add test for getLinkLabelTransform

--- test/graph/link/getLinkLabelTransform-test.js
+++ test/graph/link/getLinkLabelTransform-test.js
@@ -12,10 +12,9 @@ tape("Transform label, make label text always upward", function(test){
         .links([{id: 1, src: 1, dst: 2, direction: 1}, {id: 2, src: 2, dst: 1, direction: 1}]);
 
 
-    //test.deepEqual(myGraph.links()[0].getLinkLabelTransform(1), 1);
-    //test.deepEqual(myGraph.links()[1].getLinkLabelTransform(1), 1);
+    test.deepEqual(myGraph.links()[0].getLinkLabelTransform(2), 'translate(50.5 0) scale(0.5) translate(-50.5 0)');
+    test.deepEqual(myGraph.links()[1].getLinkLabelTransform(2), 'rotate(180 59.5 0) translate(59.5 0) scale(0.5) translate(-59.5 0)');
 
-    
 
 
 
