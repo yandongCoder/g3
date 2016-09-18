e45a7c671d4838930ff55e3ae239f485068aefc1
yandong <470604226@qq.com>
2016-09-18 10:39:24 +0800

    add vertical case for arrowPointLeft and arrowPointRight test.

--- test/graph/link/arrowPointRight-test.js
+++ test/graph/link/arrowPointRight-test.js
@@ -24,5 +24,24 @@ tape("Judge Link's arrow point to right, only if Link is one-way arrow.", functi
     test.equal(myGraph.links()[3].arrowPointLeft(), false);
     test.equal(myGraph.links()[3].arrowPointLeft(), false);
 
+    //vertical
+    myGraph = g3.graph(svg)
+        .nodes([{id: 1, x: 0, y: -10}, {id: 2, x: 0, y: 100}])
+        .links([{id: 1, src: 2, dst: 1, direction: 0}, {id: 2, src: 2, dst: 1, direction: 1}, {id: 3, src: 2, dst: 1, direction: 2}, {id: 4, src: 2, dst: 1, direction: 3}]);
+
+
+    test.equal(myGraph.links()[0].arrowPointLeft(), false);
+    test.equal(myGraph.links()[0].arrowPointRight(), false);
+
+    test.equal(myGraph.links()[1].arrowPointLeft(), false);
+    test.equal(myGraph.links()[1].arrowPointRight(), false);
+
+    test.equal(myGraph.links()[2].arrowPointLeft(), false);
+    test.equal(myGraph.links()[2].arrowPointRight(), false);
+
+    test.equal(myGraph.links()[3].arrowPointLeft(), false);
+    test.equal(myGraph.links()[3].arrowPointLeft(), false);
+    test.end();
+
     test.end();
 });
