b69eed6b635da4d58b872798e2c1fc9083a8d2b2
yandong <470604226@qq.com>
2016-09-22 15:10:18 +0800

    add more test in getHomoLinks

--- test/graph/link/getHomoLinks-test.js
+++ test/graph/link/getHomoLinks-test.js
@@ -16,3 +16,28 @@ tape("Return all Links that connected same source and target as received Link", 
 
     test.end();
 });
+
+tape("new Link merged by original Links also returned by getHomoLinks", function(test){
+    var document = jsdom.jsdom('<svg id="graph"></svg>');
+    var svg = document.querySelector("#graph");
+
+    var myGraph = g3.graph(svg)
+        .nodes([{id: 1}, {id: 2}, {id: 3}])
+        .links([{id:1, src: 1, dst: 2}, {id:2, src: 1, dst: 2}, {id:3, src: 2, dst: 1}, {id:4, src: 3, dst: 2}]);
+
+    test.equal(myGraph.links()[0].getHomoLinks().length, 3);
+
+    myGraph.links()[0].merge();
+    test.equal(myGraph.links()[0].getHomoLinks().length, 4);
+
+    myGraph.links({id: 5, src: 1, dst: 2});
+    test.equal(myGraph.links()[0].getHomoLinks().length, 5);
+
+    myGraph.links()[4].merge();
+    myGraph.links()[1].merge();//idempotent (幂等性)
+
+    test.equal(myGraph.links()[0].getHomoLinks().length, 6);
+    test.deepEqual(myGraph.links()[0].getHomoLinks(), myGraph.links()[6].getHomoLinks());
+
+    test.end();
+});
