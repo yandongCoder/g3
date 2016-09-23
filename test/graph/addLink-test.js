ec5da932c37ffb8ebb10932f72d14130c1339836
yandong <470604226@qq.com>
2016-09-23 17:37:57 +0800

    change test of addLink and addNode

---
+++ test/graph/addLink-test.js
@@ -0,0 +1,30 @@
+var tape = require("tape"),
+    jsdom = require("jsdom"),
+    g3 = require("../../dist/js/g3");
+
+tape("Push new link to graph's links", function(test){
+    var document = jsdom.jsdom('<svg id="graph"></svg>');
+    var svg = document.querySelector("#graph");
+    global.window = document.defaultView;
+    global.document = document;
+
+
+    var myGraph = g3.graph(svg);
+
+    //last could not add
+    myGraph.nodes([{id: 1}, {id: 2}])
+        .links({id: 1, src: 1, dst: 2});
+    test.equal(myGraph.links().length, 1);
+
+
+    //could not add
+    myGraph._addLink({id: 1, src: 1, dst: 2});
+    test.equal(myGraph.links().length, 1);
+
+
+    //could add
+    myGraph._addLink({id: 2, src: 1, dst: 2});
+    test.equal(myGraph.links().length, 2);
+
+    test.end();
+});
--- test/graph/addNode-test.js
+++ test/graph/addNode-test.js
@@ -2,7 +2,7 @@ var tape = require("tape"),
     jsdom = require("jsdom"),
     g3 = require("../../dist/js/g3");
 
-tape("push new nodes to graph's nodes", function(test){
+tape("Push new node to graph's nodes", function(test){
     var document = jsdom.jsdom('<svg id="graph"></svg>');
     var svg = document.querySelector("#graph");
     global.window = document.defaultView;
@@ -17,12 +17,12 @@ tape("push new nodes to graph's nodes", function(test){
 
 
     //could not add
-    myGraph.addNode({id: 2, label: "X", x: 100, y: 120});
+    myGraph._addNode({id: 2, label: "X", x: 100, y: 120});
     test.equal(myGraph.nodes().length, 2);
 
     
     //could add
-    myGraph.addNode({id: 3, label: "X", x: 100, y: 120});
+    myGraph._addNode({id: 3, label: "X", x: 100, y: 120});
     test.equal(myGraph.nodes().length, 3);
 
     test.end();
