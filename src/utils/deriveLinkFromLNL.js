2dc1b1d2451d6c873367a648f05bf3d8fc1494d5
yandong <470604226@qq.com>
2016-09-24 18:01:41 +0800

    Add deriveLinkFromLNL and test.

---
+++ src/utils/deriveLinkFromLNL.js
@@ -0,0 +1,19 @@
+import Link from "../graph/link/index";
+import deriveLinkFromLinks from "./deriveLinkFromLinks";
+import direction from "../utils/deriveDirection";
+
+export default function (srcLinks, Node, dstLinks) {
+    srcLinks = srcLinks.length > 1? new Link(deriveLinkFromLinks(srcLinks)): srcLinks[0];
+    dstLinks = dstLinks.length > 1? new Link(deriveLinkFromLinks(dstLinks)): dstLinks[0];
+
+    var obj = {};
+    obj.id = "derived:(" + srcLinks.id + ")" + Node.id + "(" + dstLinks.id + ")";
+    obj.label = "(" + srcLinks.label() + ")" + Node.label() + "(" + dstLinks.label() + ")";
+    obj.src = srcLinks.src === Node.id? srcLinks.dst: srcLinks.src;
+    obj.dst = dstLinks.src === Node.id? dstLinks.dst: dstLinks.src;
+    obj.width = (srcLinks.width() + dstLinks.width()) / 2;
+    obj.color = Node.color();
+    obj.direction = direction([srcLinks, dstLinks]);
+
+    return obj;
+}
---
+++ test/utils/deriveLinkFromLNL-test.js
@@ -0,0 +1,24 @@
+var tape = require("tape"),
+    jsdom = require("jsdom"),
+    g3 = require("../../dist/js/g3");
+
+
+tape("derive new Link from received Links&Node&Links", function(test){
+    var document = jsdom.jsdom('<svg id="graph"></svg>');
+    var svg = document.querySelector("#graph");
+
+    var myGraph = g3.graph(svg)
+        .nodes([{id: 1, label: "a", size: 50}, {id: 2, label: "b", color: "#445987"}, {id:3, label: "c"}])
+        .links([{id: 1, src: 1, dst: 2, label: "L1", color: "#ff0000", width: 5, direction: 1}, {id: 2, src: 2, dst: 1, label: "L2", color: "#00ff00", direction: 2},  {id: 3, src: 2, dst: 3, label: "L3", color: "#00ff00", direction: 2}]);
+
+    var linkObj = g3.utils.deriveLinkFromLNL([myGraph.links()[0], myGraph.links()[1]], myGraph.nodes()[1], [myGraph.links()[2]]);
+    test.equal(linkObj.id, "derived:(derived:1&2)2(3)");
+    test.equal(linkObj.label, "(L1&L2)b(L3)");
+    test.equal(linkObj.src, myGraph.nodes()[0].id);
+    test.equal(linkObj.dst, myGraph.nodes()[2].id);
+    test.equal(linkObj.width, 3.5);
+    test.equal(linkObj.color, myGraph.nodes()[1].color());
+    test.equal(linkObj.direction, 3);
+
+    test.end();
+});
