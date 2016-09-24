5a865c7f9f4906827e474b4fe516c77fcf162027
yandong <470604226@qq.com>
2016-09-24 14:18:27 +0800

    if grouped is true, getConnectedLinks return grouped Links array

--- src/graph/node/getConnectedLinks.js
+++ src/graph/node/getConnectedLinks.js
@@ -1,5 +1,22 @@
-export default function () {
-    return this.graph._links.filter(function (Link) {
+export default function (grouped) {
+    var connectedLinks = this.graph._links.filter(function (Link) {
         return  ((Link.source === this) || (Link.target === this)) && !Link.merged();
     }, this);
+
+    if(grouped){
+        var separated = {};
+
+        connectedLinks.forEach(function(Link){
+            var separatedId = Link.src === this.id? Link.dst: Link.src;
+            if(separated[separatedId] === undefined) separated[separatedId] = [];
+            separated[separatedId].push(Link);
+        },this);
+
+        var connectedLinks = [];
+        for (var k in separated){
+            connectedLinks.push(separated[k]);
+        }
+    }
+
+    return connectedLinks;
 }
--- test/graph/node/getConnectedLinks-test.js
+++ test/graph/node/getConnectedLinks-test.js
@@ -8,18 +8,15 @@ tape("Get connected links of Node", function(test){
 
     var myGraph = g3.graph(svg);
 
-
     myGraph.nodes([{id: 1}, {id: 2},{id: 3}]);
     var links = [{id: 1, src: 1, dst: 2}, {id: 2, src: 2, dst: 3}];
     myGraph.links(links);
-
 
     test.deepEqual(myGraph.nodes()[0].getConnectedLinks(), [myGraph.links()[0]]);
     test.deepEqual(myGraph.nodes()[1].getConnectedLinks(), [myGraph.links()[0], myGraph.links()[1]]);
     test.deepEqual(myGraph.nodes()[2].getConnectedLinks(), [myGraph.links()[1]]);
 
     test.end();
-
 
 });
 
@@ -28,7 +25,6 @@ tape("If connected links is merged, get the derived Link.", function(test){
     var svg = document.querySelector("#graph");
 
     var myGraph = g3.graph(svg);
-
 
     myGraph.nodes([{id: 1}, {id: 2},{id: 3}]);
     var links = [{id: 1, src: 1, dst: 2}, {id: 2, src: 2, dst: 3}, {id: 3, src: 3, dst: 2}];
@@ -46,5 +42,24 @@ tape("If connected links is merged, get the derived Link.", function(test){
 
     test.end();
 
+});
 
+tape("If grouped argument is true, return Links grouped by 'between Nodes'", function(test){
+    var document = jsdom.jsdom('<svg id="graph"></svg>');
+    var svg = document.querySelector("#graph");
+
+    var myGraph = g3.graph(svg);
+
+    myGraph.nodes([{id: 1}, {id: 2},{id: 3}]);
+    var links = [{id: 1, src: 1, dst: 2}, {id: 2, src: 2, dst: 3}, {id: 3, src: 3, dst: 2}];
+    myGraph.links(links);
+
+    test.deepEqual(myGraph.nodes()[0].getConnectedLinks(true), [[myGraph.links()[0]]]);
+    test.deepEqual(myGraph.nodes()[1].getConnectedLinks(true), [[myGraph.links()[0]], [myGraph.links()[1], myGraph.links()[2]]]);
+    test.deepEqual(myGraph.nodes()[2].getConnectedLinks(true), [[myGraph.links()[1], myGraph.links()[2]]]);
+
+    myGraph.links()[2].merge();
+    test.deepEqual(myGraph.nodes()[1].getConnectedLinks(true), [[myGraph.links()[0]], [myGraph.links()[3]]]);
+
+    test.end();
 });
