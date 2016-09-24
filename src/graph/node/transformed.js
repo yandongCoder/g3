1f36c6e93525c0c421ee3bd129e33642f1ee1c66
yandong <470604226@qq.com>
2016-09-24 18:14:53 +0800

    add transformed method for Link and Node.

---
+++ src/graph/node/transformed.js
@@ -0,0 +1,7 @@
+export default function (transformed) {
+    if(!arguments.length) return this._transformed || false;
+
+    this._transformed = transformed;
+    
+    return this;
+}
---
+++ test/graph/link/transformed-test.js
@@ -0,0 +1,19 @@
+var tape = require("tape"),
+    jsdom = require("jsdom"),
+    g3 = require("../../../dist/js/g3");
+
+tape("Transform Link should mark this Link's _transform true.", function(test){
+    var document = jsdom.jsdom('<svg id="graph"></svg>');
+    var svg = document.querySelector("#graph");
+
+    var myGraph = g3.graph(svg)
+        .nodes([{id: 1}, {id: 2}, {id: 3}])
+        .links([{id:1, src: 1, dst: 2}]);
+
+
+    test.equal(myGraph.links()[0].transformed(), false);
+    myGraph.links()[0].transformed(true);
+    test.equal(myGraph.links()[0]._transformed, true);
+
+    test.end();
+});
