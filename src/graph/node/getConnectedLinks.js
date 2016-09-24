bd334170daf7f20a73804df8079e8ab055218d0d
yandong <470604226@qq.com>
2016-09-24 12:36:36 +0800

    getConnectedLinks return derived Link rather than merged Links

--- src/graph/node/getConnectedLinks.js
+++ src/graph/node/getConnectedLinks.js
@@ -1,5 +1,5 @@
 export default function () {
     return this.graph._links.filter(function (Link) {
-        return  (Link.source === this) || (Link.target === this);
+        return  ((Link.source === this) || (Link.target === this)) && !Link.merged();
     }, this);
 }
--- test/graph/node/getConnectedLinks-test.js
+++ test/graph/node/getConnectedLinks-test.js
@@ -2,21 +2,47 @@ var tape = require("tape"),
     jsdom = require("jsdom"),
     g3 = require("../../../dist/js/g3");
 
-tape("get connected links by Node", function(test){
+tape("Get connected links of Node", function(test){
     var document = jsdom.jsdom('<svg id="graph"></svg>');
     var svg = document.querySelector("#graph");
 
     var myGraph = g3.graph(svg);
 
 
-    myGraph.nodes([{id: 1}, {id: 2},{id: 3},{id: 4}, {id: 5},{id: 6}]);
-    var links = [{id: 1, src: 1, dst: 2}, {id: 2, src: 2, dst: 3}, {id: 3, src: 1, dst: 5}, {id: 4, src: 5, dst: 6}, {id: 5, src: 5, dst: 2}];
+    myGraph.nodes([{id: 1}, {id: 2},{id: 3}]);
+    var links = [{id: 1, src: 1, dst: 2}, {id: 2, src: 2, dst: 3}];
     myGraph.links(links);
 
-    test.equal(myGraph.links().length, 5);
-    
-    //by Node
-    test.deepEqual(myGraph.nodes()[2].getConnectedLinks()[0], myGraph.links()[1]);
+
+    test.deepEqual(myGraph.nodes()[0].getConnectedLinks(), [myGraph.links()[0]]);
+    test.deepEqual(myGraph.nodes()[1].getConnectedLinks(), [myGraph.links()[0], myGraph.links()[1]]);
+    test.deepEqual(myGraph.nodes()[2].getConnectedLinks(), [myGraph.links()[1]]);
+
+    test.end();
+
+
+});
+
+tape("If connected links is merged, get the derived Link.", function(test){
+    var document = jsdom.jsdom('<svg id="graph"></svg>');
+    var svg = document.querySelector("#graph");
+
+    var myGraph = g3.graph(svg);
+
+
+    myGraph.nodes([{id: 1}, {id: 2},{id: 3}]);
+    var links = [{id: 1, src: 1, dst: 2}, {id: 2, src: 2, dst: 3}, {id: 3, src: 3, dst: 2}];
+    myGraph.links(links);
+
+    test.deepEqual(myGraph.nodes()[0].getConnectedLinks(), [myGraph.links()[0]]);
+    test.deepEqual(myGraph.nodes()[1].getConnectedLinks(), [myGraph.links()[0], myGraph.links()[1], myGraph.links()[2]]);
+    test.deepEqual(myGraph.nodes()[2].getConnectedLinks(), [myGraph.links()[1], myGraph.links()[2]]);
+
+    myGraph.links()[2].merge();
+
+    test.deepEqual(myGraph.nodes()[0].getConnectedLinks(), [myGraph.links()[0]]);
+    test.deepEqual(myGraph.nodes()[1].getConnectedLinks(), [myGraph.links()[0], myGraph.links()[3]]);
+    test.deepEqual(myGraph.nodes()[2].getConnectedLinks(), [myGraph.links()[3]]);
 
     test.end();
 
