6d34ec9a41d47fa100f88c4bf33fc1fb4efd7e04
yandong <470604226@qq.com>
2016-09-25 18:31:31 +0800

    addLink return Link self

--- src/graph/addLink.js
+++ src/graph/addLink.js
@@ -3,4 +3,6 @@ import Link from "./link/index";
 export default function (obj) {
     var link = new Link(obj, this);
     if(!this.hasLink(link) && link.hasST()) this._links.push(link);
+
+    return link;
 }
