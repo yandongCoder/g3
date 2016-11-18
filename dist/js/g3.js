a24bfabbf05945fafab86fd28ed938739f675c94
yandong <470604226@qq.com>
2016-11-18 10:30:13 +0800

    Add keyBoard event "ctrl+a" and "delete";

--- dist/js/g3.js
+++ dist/js/g3.js
@@ -1840,13 +1840,20 @@ function translateBy(x, y, duration) {
 }
 
 function keydowned() {
-    
+    d3.event.preventDefault();
     if (!d3.event.metaKey) {
         switch (d3.event.keyCode) {
             //shift alt and space is used by d3 brush
             case 90:
                 this.brush.show();
                 break;
+            case 46:
+            case 8:
+                this.removeNodes(this.getSelectedNodes());
+            break;
+            case 65:
+                if(d3.event.ctrlKey) this.selectNodes(this.getNodes());
+            break;
         }
     }
 }
