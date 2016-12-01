66c27796ebd328feff40220a553d022986b55064
yandong <470604226@qq.com>
2016-12-01 18:32:17 +0800

    Update README

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# graph
-graph visualization and graph algorithms based on D3.js
+# g3
+基于D3的图可视化库。
+
+##特性
 
 #Temporary Live Demo
 [jsfiddle Demo](https://jsfiddle.net/yandongCoder/f5tmfq3j/)
+
+<script async src="//jsfiddle.net/yandongCoder/f5tmfq3j/embed/"></script>
 
 //link, node obj, 保留字 source
--- dist/js/g3.js
+++ dist/js/g3.js
@@ -364,10 +364,8 @@ function Link(data, graph) {
     this.source = graph && this.graph._nodesHash[data.src];
     this.target = graph && this.graph._nodesHash[data.dst];
     
-    
-    var exceptKey = ['src', 'dst'];
     for (var prop in data) {
-        if (data.hasOwnProperty(prop) && this[prop] === undefined && exceptKey.indexOf(prop) === -1) this[prop] = data[prop];
+        if (data.hasOwnProperty(prop) && this[prop] === undefined) this[prop] = data[prop];
     }
 }
 
