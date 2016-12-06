dcd9797d5c6392a2cd4ec9367f72c729388d2a83
yandong <470604226@qq.com>
2016-12-06 10:25:35 +0800

    Add focus to setTimeout, to void execute before svg render.

--- src/graph/transform.js
+++ src/graph/transform.js
@@ -18,31 +18,34 @@ function translateBy(x, y, duration) {
 }
 
 function focus(filter, duration){
-    var Nodes = this.getNodes(filter);
-    if(!Nodes.length) return;
+    setTimeout(function(){
+        var Nodes = this.getNodes(filter);
+        if(!Nodes.length) return;
     
-    var xAccessor = function(Node){return Node.x}, yAccessor = function(Node){return Node.y};
-    var minX = d3.min(Nodes, xAccessor), maxX = d3.max(Nodes, xAccessor), minY = d3.min(Nodes, yAccessor), maxY = d3.max(Nodes, yAccessor);
-    var xSpan = maxX - minX, ySpan = maxY - minY;
-    var xCenter = (maxX + minX) / 2, yCenter = (maxY + minY) / 2;
-    var canvasW = this.canvas.width.baseVal.value,
-        canvasH = this.canvas.height.baseVal.value;
+        var xAccessor = function(Node){return Node.x}, yAccessor = function(Node){return Node.y};
+        var minX = d3.min(Nodes, xAccessor), maxX = d3.max(Nodes, xAccessor), minY = d3.min(Nodes, yAccessor), maxY = d3.max(Nodes, yAccessor);
+        var xSpan = maxX - minX, ySpan = maxY - minY;
+        var xCenter = (maxX + minX) / 2, yCenter = (maxY + minY) / 2;
+        var canvasW = this.canvas.width.baseVal.value,
+            canvasH = this.canvas.height.baseVal.value;
     
-    var xScale = canvasW / xSpan,
-        yScale = canvasH / ySpan;
+        var xScale = canvasW / xSpan,
+            yScale = canvasH / ySpan;
     
-    var scale = d3.min([xScale, yScale]);
-    if(scale > this._config.maxScale) scale = this._config.maxScale;
-    scale = scale === Infinity? 1: scale;
-    scale -= 0.1;
+        var scale = d3.min([xScale, yScale]);
+        if(scale > this._config.maxScale) scale = this._config.maxScale;
+        scale = scale === Infinity? 1: scale;
+        scale -= 0.1;
     
     
-    var transformed = d3.zoomIdentity
-        .translate(canvasW / 2, canvasH / 2)
-        .scale(scale)
-        .translate(-xCenter, -yCenter);
+        var transformed = d3.zoomIdentity
+            .translate(canvasW / 2, canvasH / 2)
+            .scale(scale)
+            .translate(-xCenter, -yCenter);
     
-    this._getSvgSelection(duration || 1000).call(this.zoom.transform, transformed);
+        console.log(canvasW, canvasH);
+        this._getSvgSelection(duration || 1000).call(this.zoom.transform, transformed);
+    }.bind(this), 0)
 }
 
 export {transform, scaleTo, translateBy, focus};
